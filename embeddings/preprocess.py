9c447256dc8114a348b15183f1cbc7ddd22a6d95
Bhushan Gunjal <79009485+BhushanGunjal@users.noreply.github.com>
2025-01-07 23:55:09 +0530

    Streamlit error fix-2

--- embeddings/preprocess.py
+++ embeddings/preprocess.py
@@ -1,9 +1,44 @@
 import nltk
+import os
+
+# Specify the path to store NLTK data
+nltk_data_path = os.path.join(os.getcwd(), 'nltk_data')
+
+# Ensure NLTK data directory is added to the NLTK data path list
+nltk.data.path.append(nltk_data_path)
+
+# Download the necessary resources if not found
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords', download_dir=nltk_data_path)
+
+try:
+    nltk.data.find('corpora/wordnet')
+except LookupError:
+    nltk.download('wordnet', download_dir=nltk_data_path)
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', download_dir=nltk_data_path)
+
+
+
+
+
+
+
+
+
+
+
+# import nltk
 
 # Download necessary NLTK resources (should be downloaded once)
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('stopwords')
+# nltk.download('punkt')
+# nltk.download('wordnet')
+# nltk.download('stopwords')
 
 
 from nltk.tokenize import word_tokenize
